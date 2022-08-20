7beb500eada987a9f41511b2135219b51f9fb262
TheRick88 <avalex2810@gmail.com~>
2022-08-21 01:06:57 +0200

    countwatch function fixed

--- init.js
+++ init.js
@@ -36,21 +36,94 @@ timerRef.innerHTML = ` ${m} : ${s} : ${ms}`;
 
 // FUNCTION FOR THE COUNT-DOWN
 
-const input = document.getElementById('userInput')
+// const hoursEl = document.querySelector('#hours');
+// const minutesEl = document.querySelector('#minutes');
+// const secondsEl = document.querySelector('#seconds');
+// const btnStart = document.querySelector('.btn-start-resume');
+// const btnPause = document.querySelector('.btn-pause');
+// const btnStop = document.querySelector('.btn-stop');
+// const btnReset = document.querySelector('.btn-reset');
+// let interval;
+// let pause = false;
+// let totalSeconds = 0;
+// let totalSecondsBackup = 0;
 
-const startingMinutes = input.value;
-let time = startingMinutes * 60;
+// init();
 
-const countDownEl = document.getElementById('countdown');
+// function init() {
+//     btnPause.style.display = 'none';
+//     btnStop.style.display = 'none';
+//     btnReset.style.display = 'none';
 
-setInterval(updateCountdown, 1000);
 
-function updateCountdown () {
-  const minutes = Math.floor(time/ 60);
-  let seconds = time % 60;
+//     btnStart.addEventListener('click', () => {
+//         const hours = parseInt(hoursEl.value);
+//         const minutes = parseInt(minutesEl.value);
+//         const seconds = parseInt(secondsEl.value);
 
-  seconds = seconds < 10 ? '0' + seconds : seconds;
+//         totalSecondsBackup = totalSeconds = hours * 60 * 60 + minutes * 60 + seconds;
+//         if (totalSeconds < 0) {
+//             return;
+//         }
 
-  countDownEl.innerHTML = `${minutes}:${seconds}`;
-  time--;
-}
+//         startTimer();
+
+//         btnPause.style.display = 'inline-block';
+//         btnStop.style.display = 'inline-block';
+//         btnReset.style.display = 'inline-block';
+//         btnStart.style.display = 'none';
+//     });
+
+//     btnPause.addEventListener('click', () => {
+//         pause = !pause;
+//         if (pause) {
+//             btnPause.innerText = 'Resume';
+//         } else {
+//             btnPause.innerText = 'Pause';
+//         }
+//     })
+
+//     btnStop.addEventListener('click', () => {
+//         stopTimer();
+//         totalSeconds = totalSecondsBackup;
+//         pause = false;
+//         updateInputs();
+
+//         btnPause.style.display = 'none';
+//         btnStop.style.display = 'none';
+//         btnReset.style.display = 'none';
+//         btnStart.style.display = '';
+//     })
+
+//     btnReset.addEventListener('click', () => {
+//         totalSeconds = totalSecondsBackup;
+//         updateInputs();
+//     })
+// }
+
+// function startTimer() {
+//     interval = setInterval(() => {
+
+//         if (pause) return;
+//         totalSeconds--;
+//         updateInputs();
+
+//         if (totalSeconds <= 0) {
+//             stopTimer();
+//         }
+//     }, 1000)
+// }
+
+// function stopTimer() {
+//     interval = clearInterval(interval);
+// }
+
+// function updateInputs() {
+//     const hours = Math.floor(totalSeconds / 60 / 60);
+//     const minutes = Math.floor(totalSeconds / 60);
+//     const seconds = totalSeconds % 60;
+
+//     hoursEl.value = hours;
+//     minutesEl.value = minutes;
+//     secondsEl.value = seconds;
+// }
